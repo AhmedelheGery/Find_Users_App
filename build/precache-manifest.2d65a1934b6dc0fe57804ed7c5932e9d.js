3b5435fbfe22aba4140531a857d92870d5893718
ahmed <Ahmed88alaa>
2019-08-21 16:36:58 +0200

    creat react app repo

---
+++ build/precache-manifest.2d65a1934b6dc0fe57804ed7c5932e9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3db975fe62e7cbf305e2310f4db75171",
+    "url": "/AhmedelheGery/React-App.git/index.html"
+  },
+  {
+    "revision": "a1159a69308a37e946a0",
+    "url": "/AhmedelheGery/React-App.git/static/css/main.c38b2907.chunk.css"
+  },
+  {
+    "revision": "e119d68e7adb1ecc22f4",
+    "url": "/AhmedelheGery/React-App.git/static/js/2.c0e01b8b.chunk.js"
+  },
+  {
+    "revision": "a1159a69308a37e946a0",
+    "url": "/AhmedelheGery/React-App.git/static/js/main.f8b4dd94.chunk.js"
+  },
+  {
+    "revision": "14e6dc7a1c72c4d76425",
+    "url": "/AhmedelheGery/React-App.git/static/js/runtime~main.9683d492.js"
+  }
+]);
